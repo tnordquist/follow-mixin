623c17961513514ff01ec8505b17559ea59a76f4
Todd A Nordquist <toddnordquist144@gmail.com>
2019-09-19 11:25:25 -0600

    Added add class and repository elements.

--- src/main/java/edu/cnm/deepdive/follow/entity/Person.java
+++ src/main/java/edu/cnm/deepdive/follow/entity/Person.java
@@ -3,14 +3,11 @@ package edu.cnm.deepdive.follow.entity;
 import java.util.Set;
 import java.util.TreeSet;
 import java.util.UUID;
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
-import javax.persistence.OneToMany;
-import org.hibernate.annotations.GenericGenerator;
 
 @Entity
 public class Person implements Comparable {
@@ -25,16 +22,12 @@ public class Person implements Comparable {
   @GeneratedValue
   private UUID id;
 
-  //  @Column(name = "followers_id", columnDefinition = "CHAR(16) FOR BIT DATA",
-  //      nullable = false, updatable = false)
-  //  private UUID id;
-
   private String firstName;
   private String lastName;
 
-//  @OneToMany(mappedBy = "followers", fetch = FetchType.EAGER)
-//  private Set<Person> followers = new TreeSet<>();
-//
+  @ManyToMany(mappedBy = "followers", fetch = FetchType.EAGER)
+  private Set<Person> followers = new TreeSet<>();
+
   @ManyToMany(mappedBy = "follows", fetch = FetchType.EAGER)
   private Set<Person> follows = new TreeSet<>();
 
@@ -61,5 +54,13 @@ public class Person implements Comparable {
 
   public void setId(UUID id) {
     this.id = id;
+  }
+
+  public Set<Person> getFollowers() {
+    return followers;
+  }
+
+  public Set<Person> getFollows() {
+    return follows;
   }
 }
--- src/main/java/edu/cnm/deepdive/follow/repository/PersonRepository.java
+++ src/main/java/edu/cnm/deepdive/follow/repository/PersonRepository.java
@@ -2,10 +2,9 @@ package edu.cnm.deepdive.follow.repository;
 
 import edu.cnm.deepdive.follow.entity.Person;
 import java.util.UUID;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
-public interface PersonRepository extends JpaRepository<Person, UUID> {
+public interface PersonRepository extends CrudRepository<Person, UUID> {
 
 
 
