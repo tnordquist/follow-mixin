bb5f8bb743f7ec9956f6dc42f1a4c039faa8b679
Todd A Nordquist <toddnordquist144@gmail.com>
2019-09-19 11:26:28 -0600

    Deleted AddPersonRequest

--- src/main/java/edu/cnm/deepdive/follow/controller/PersonController.java
+++ src/main/java/edu/cnm/deepdive/follow/controller/PersonController.java
@@ -1,11 +1,9 @@
 package edu.cnm.deepdive.follow.controller;
 
 import edu.cnm.deepdive.follow.entity.Person;
-import edu.cnm.deepdive.follow.entity.request.AddPersonRequest;
 import edu.cnm.deepdive.follow.repository.PersonRepository;
 import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,14 +22,6 @@ public class PersonController {
   @RequestMapping(method = RequestMethod.GET)
   public Set<Person> findAllPersons() {
     return (Set<Person>) personRepository.findAll();
-  }
-
-  @RequestMapping(method = RequestMethod.POST)
-  public void addPerson(@RequestBody AddPersonRequest addPersonRequest){
-    Person person = new Person();
-    person.setFirstName(addPersonRequest.getFirstName());
-    person.setLastName(addPersonRequest.getLastName());
-    personRepository.save(person);
   }
 
 
